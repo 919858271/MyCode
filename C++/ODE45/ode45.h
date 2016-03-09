7e4bcf2de95cd9c816b0edada6b4d70e4f5f7f9a
jianwen xu <919858271@qq.com>
2016-03-09 13:12:24 +0800

    update files

---
+++ C++/ODE45/ode45.h
@@ -0,0 +1,15 @@
+/*!
+ * \file ode45.h
+ * \date 2015/12/25 9:19
+ *
+ * \author jianwenxu
+ * Contact: 919858271@qq.com
+ *
+ * \brief 
+ *
+ * TODO: 
+ *
+ * \note
+ */
+#include "fun.h"
+void ode45(int iterTimes,int dimension,double *initValue,double tPace,double **result);
