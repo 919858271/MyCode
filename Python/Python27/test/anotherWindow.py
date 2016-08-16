c04e7d44c7504249d47785fec436a5bc0458b2d7
jianwen xu <919858271@qq.com>
2016-08-16 20:33:54 +0800

    Revert "python"
    This reverts commit 110c533115923b1127e6cc6ec00a3d135c4826c8.

--- Python/Python27/test/anotherWindow.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-
-from PyQt4.QtGui import QMainWindow
-
-class AnotherWindow(QMainWindow):
-    def __init__(self):
-        super(AnotherWindow, self).__init__()
-        self.resize(400, 300)
-        self.setWindowTitle("this is another window")
