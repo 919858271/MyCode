8d17ee9f84e5f002b51197cbba32ba18293c83cb
jianwenxu <npujianwenxu@163.com>
2018-08-18 22:06:15 +0800

    python

--- Python/Spyder/spyder.py
+++ Python/Spyder/spyder.py
@@ -4,3 +4,14 @@
 # Author:      jianwen
 # Email:       npujianwenxu@163.com
 #-------------------------------------------------------------------------------
+from urllib import request
+url = u'http://www.xicidaili.com/nn/'
+headers = {
+    'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
+    'Referer': url
+}
+
+req = request.Request(url, headers=headers)
+res = request.urlopen(req)
+html = res.read().decode('utf-8')
+print(html)
