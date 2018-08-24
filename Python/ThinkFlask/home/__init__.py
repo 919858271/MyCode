c1ed44234eff285d661a2685a91e435d6025d026
jianwenxu <npujianwenxu@163.com>
2018-08-24 20:21:24 +0800

    python

--- Python/ThinkFlask/home/__init__.py
+++
@@ -1,7 +0,0 @@
-#-------------------------------------------------------------------------------
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Author:      jianwen
-# Email:       npujianwenxu@163.com
-#-------------------------------------------------------------------------------
-import home.index  #This is your view file.
