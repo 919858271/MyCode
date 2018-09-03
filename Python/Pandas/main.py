b509731cccec941a429bd57d3602946b27d7aa1c
jianwenxu <npujianwenxu@163.com>
2018-09-03 22:20:45 +0800

    modify py

--- Python/Pandas/main.py
+++ Python/Pandas/main.py
@@ -5,3 +5,20 @@
 #-------------------------------------------------------------------------------
 import pandas as pd
 import numpy as np
+
+file_path = '1.csv'
+
+
+class CSV():
+    def __init__(self, csv_file_path):
+        self.df = pd.read_csv(csv_file_path)
+
+    def select(self, by_field=[]):
+        return self.df[by_field] if by_field else False
+
+
+data = CSV(file_path)
+field = ['v1', 'v2']
+
+v1 = data.select(field)
+print(v1)
