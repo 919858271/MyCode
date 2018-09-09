61106e08eb156c300b3bb42b6ddcf2c9e900093e
jianwenxu <npujianwenxu@163.com>
2018-09-09 16:50:41 +0800

    add file

---
+++ Python/PyUnit/Config/config.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author:      jianwen
+# Email:       npujianwenxu@163.com
+#-------------------------------------------------------------------------------
+TEST_CASE_PATH = '/TestCase/'
+REPORT_PATH = '/Report/'
+FILE_PREFIX = 'Report_'
+FILE_SUFFIX = '.html'
+TEST_CASE_PATTERN = 'test*.py'
