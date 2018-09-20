7cf269454fa562827b0318e385d0966aa7941baf
jianwenxu <npujianwenxu@163.com>
2018-09-21 00:00:24 +0800

    modify manage.py

--- Python/ThinkFlask/manage.py
+++ Python/ThinkFlask/manage.py
@@ -2,18 +2,20 @@
 # -*- coding: utf-8 -*-
 # Author:      jianwen
 # Email:       npujianwenxu@163.com
-
 from flask_script import Manager, Server
-import app
+from flask_migrate import Migrate, MigrateCommand
+from app import app, db
 
-manager = Manager(app.app)
+manager = Manager(app)
+migrate = Migrate(app, db)
 
+manager.add_command('db', MigrateCommand)
 manager.add_command(
     "runserver",
     Server(
-        host=app.app.config['HOST'],
-        port=app.app.config['PORT'],
-        use_debugger=app.app.config['DEBUG']))
+        host=app.config['HOST'],
+        port=app.config['PORT'],
+        use_debugger=app.config['DEBUG']))
 
 
 @manager.shell
@@ -21,7 +23,7 @@ def make_shell_context():
     '''
     make sure Flask app object imported，otherwise the  app object not in the started CLI
     '''
-    return dict(app=app.app, db=app.db)
+    return dict(app=app, db=db)
 
 
 if __name__ == '__main__':
