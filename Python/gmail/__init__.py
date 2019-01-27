bc0071ee8635c5eb2bb1f172c4c8d7c23e81eaf7
jianwenxu <npujianwenxu@163.com>
2019-01-27 11:09:47 +0800

    add gmail from yagmail

---
+++ Python/gmail/__init__.py
@@ -0,0 +1,7 @@
+from .error import ConnectionClosed
+from .error import AddressError
+from .password import register
+from .sender import SMTP
+from .sender import logging
+from .utils import raw
+from .utils import inline
